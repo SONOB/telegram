b0cf26acbbcdd22e1390e3cfe90bd2b06ce8c78c
이용진 <118358452+SONOB@users.noreply.github.com>
2023-10-07 01:25:09 +0900

    echobot.py

---
+++ echobot.py
@@ -0,0 +1,28 @@
+#텔레그램에서 적은 문장을 다시 돌려보내는 에코봇입니다.
+#logging의 경우 터미널창에서 텔레그램이 실행되고 있는 것을 보여주는 코드이기에 빼셔도 무관합니다.
+
+import logging
+from telegram import Update
+from telegram.ext import Application, ContextTypes, MessageHandler, filters
+
+token = '6176833236:AAGoDeXjeO7KoVL2_zT3pBfaJkLvf01YDvA'
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+def main():
+
+    application = Application.builder().token(token).build()
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
