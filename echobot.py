54ad22ef331002e143472833f9aa877c2fdcaa7a
이용진 <118358452+SONOB@users.noreply.github.com>
2023-10-07 01:59:41 +0900

    Echobot.py

--- echobot.py
+++ echobot.py
@@ -8,7 +8,6 @@ from telegram.ext import Application, ContextTypes, MessageHandler, filters
 token = '본인의 Token값을 넣으시면 됩니다.'
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
@@ -16,11 +15,9 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
 def main():
-
     application = Application.builder().token(token).build()
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 if __name__ == "__main__":
     main()
